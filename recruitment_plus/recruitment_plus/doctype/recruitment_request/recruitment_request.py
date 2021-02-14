be1cc93946391d6348c09e347c96419536e0ed8a
jiloysss <jangeles@bai.ph>
2021-02-14 15:48:25 +0800

    update

--- recruitment_plus/recruitment_plus/doctype/recruitment_request/recruitment_request.py
+++ recruitment_plus/recruitment_plus/doctype/recruitment_request/recruitment_request.py
@@ -11,3 +11,16 @@ class RecruitmentRequest(Document):
 
 		frappe.db.sql(""" UPDATE `tabRecruitment Request` SET status=%s, previous_status=%s WHERE name=%s """, (status, self.status, self.name))
 		frappe.db.commit()
+
+	def on_submit(self):
+		customer = frappe.get_doc("Customer", self.customer)
+
+		if not customer.mobile_no:
+			frappe.db.sql(""" UPDATE `tabCustomer` SET mobile_no=%s WHERE name=%s""",
+						  (self.telephone, self.customer))
+			frappe.db.commit()
+
+		if not customer.email_id:
+			frappe.db.sql(""" UPDATE `tabCustomer` SET email_id=%s WHERE name=%s""",
+						  (self.email, self.customer))
+			frappe.db.commit()
